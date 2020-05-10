887a412c8631a5fed0bb6bf24fb07ca398c2501f
Eduardo Antonio Jimenez Garcia <iameduard@hotmail.com>
2020-05-10 18:52:57 -0400

    Update script_schedule.py

--- script_schedule.py
+++ script_schedule.py
@@ -3,6 +3,7 @@ import json
 import csv
 import oauth2client
 import gspread
+import os
 from   oauth2client.service_account import ServiceAccountCredentials
 from datetime import date, datetime
 ##############################################
@@ -11,16 +12,16 @@ from datetime import date, datetime
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('creds02.json', scope)
 client = gspread.authorize(creds)
-sheet_schedule = client.open('schedule').worksheet('scripts')
+sheet_schedule   = client.open('schedule').worksheet('scripts')
+schedule   = sheet_schedule.get_all_records()
+sheet_executions = client.open('schedule').worksheet('executions')
+executions = sheet_executions.get_all_records()
 now = datetime.now()
-schedule = sheet_schedule.get_all_records()
 weekday=now.strftime("%a").upper()
 H24=now.strftime("%H%p")
 for script in schedule:
-    if H24 in script.get(weekday):
-        print(script['SCRIPT'],'-',str(now))
-        os.system(python3 script['SCRIPT'])
-sheet_executions = client.open('schedule').worksheet('executions')
-executions = sheet_executions.get_all_records()
-sheet_executions.update_cell(len(executions)+2, 1, str(now))
-
+	print(script)
+	if H24 in script.get(weekday):
+		job = str(datetime.now())+'|'+script['SCRIPT']
+		sheet_executions.update_cell(len(executions)+2, 1, job)
+		os.system('python3 '+script['SCRIPT'])
